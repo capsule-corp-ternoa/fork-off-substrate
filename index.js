7eb2dd0c47d0d9c26c51d1089734e9e19810b756
ghalielouarzazi@hotmail.com <ghalielouarzazi@hotmail.com>
2023-01-22 18:14:46 +0100

    bring alice back in custom forks

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_cla
  * e.g. console.log(xxhashAsHex('System', 128)).
  */
 let prefixes = ['0x26aa394eea5630e07c48ae0c9558cef7b99d880ec681799c0cf30e8886371da9' /* System.Account */];
-const skippedModulesPrefix = ['System', 'Session', 'Babe', 'Grandpa', 'GrandpaFinality', 'FinalityTracker', 'Authorship'];
+const skippedModulesPrefix = ['System', 'Session', 'Babe', 'Grandpa', 'GrandpaFinality', 'FinalityTracker', 'Authorship', "TechnicalCommittee"];
 
 async function fixParachinStates(api, forkedSpec) {
   const skippedKeys = [
